00565c06a3a565f080403e3f877d2f64ef99b6a1
tvwest <tvwest@umich.edu>
2020-08-13 16:09:17 -0400

    Short Notes

---
+++ equation.h
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+
+// This is the data structure for representing equations
+
+ // Should be a binary search tree with inner nodes as binary operators
+// and leaves as numbers or variables.
+
+// Maybe define a variable class in equation class
+
+
+
+

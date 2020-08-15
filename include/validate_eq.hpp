e92d5224affcdf27d45c517877e60bf7f7c8fee9
tvwest <tvwest@umich.edu>
2020-08-15 17:10:26 -0400

    Finished the equation validation code, need to test

--- include/validate_eq.hpp
+++ include/validate_eq.hpp
@@ -1,7 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
 
-#ifndef validate_eq_hpp
-#define validate_eq_hpp
+using namespace std;
 
-#include <stdio.h>
+/*
+ REQUIRES: base and to_remove be non-empty strings
+ MODIFIRES: erases any occurences of to_remove from base
+*/
+void remove_sub(std::string &base, std::string to_remove);
 
-#endif /* validate_eq_hpp */
+/*
+ REQUIRES: base is a non-empty string, to_remove is a valid char
+ MODIFIRES: erases any occurences of to_remove from base
+*/
+void remove_char(std::string &base, char to_remove);
+
+/*
+ REQUIRES: in is a non-empty string
+ EFFECTS: returns 0 if all symbols and functions are valid, -1 if not
+*/
+int valid_symbols(std::string in, std::vector<char> vars);
+
+/*
+ REQUIRES: in is a non-empty string
+ EFFECTS: returns 0 if parentheses are in legal arrangement, -1 if not
+*/
+int valid_syntax(std::string &in);
+
+
+/*
+ REQUIRES: in is a non-empty string
+ EFFECTS: returns 0 if equation is valid, -1 if not
+*/
+int validate_eq(std::string &in, std::vector<char> vars);
--- src/validate_eq.cpp
+++ src/validate_eq.cpp
@@ -2,11 +2,13 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <cassert>
 
 using namespace std;
 
 
 void remove_sub(std::string &base, std::string to_remove) {
+    assert(!base.empty() && !to_remove.empty());
     int sub_size = static_cast<int>(to_remove.size());
     while(base.find(to_remove) != string::npos) {
         base.erase(base.find(to_remove), sub_size);
@@ -14,6 +16,7 @@ void remove_sub(std::string &base, std::string to_remove) {
 }
 
 void remove_char(std::string &base, char to_remove) {
+    assert(!base.empty());
     while(base.find(to_remove) != string::npos) {
         base.erase(base.find(to_remove), 1);
     }
@@ -21,6 +24,8 @@ void remove_char(std::string &base, char to_remove) {
 
 
 int valid_symbols(std::string in, std::vector<char> vars) {
+    
+    assert(!in.empty());
     
     // we erase all legal phrases and chars, and if nothing is left, then all symbols are valid
     std::string in_check = in; // copy input equation
@@ -55,11 +60,38 @@ int valid_symbols(std::string in, std::vector<char> vars) {
 
 
 int valid_syntax(std::string &in) {
+    assert(!in.empty());
     // checks if parentheses are in legal arrangement
-    return -1;
+    
+    // counts parentheses from left to right, num_right cannot exceed num_left
+    int num_left = 0;
+    int num_right = 0;
+    for(auto i = 0; i < in.size(); ++i) {
+        if(in.at(i) == '(') num_left++;
+        if(in.at(i) == ')') num_right++;
+        if(num_right > num_left) {
+            std::cout << "Invalid Equation\n";
+            return -1;
+        }
+    }
+    
+    // counts parentheses from right to left, num_left cannot exceed num_right
+    num_left = 0;
+    num_right = 0;
+    for(auto i = in.size()-1; i >= 0; ++i) {
+        if(in.at(i) == '(') num_left++;
+        if(in.at(i) == ')') num_right++;
+        if(num_left > num_right) {
+            std::cout << "Invalid Equation\n";
+            return -1;
+        }
+    }
+    
+    return 0;
 }
 
 int validate_eq(std::string &in, std::vector<char> vars) {
+    assert(!in.empty());
     if(valid_symbols(in, vars) == 0 && valid_syntax(in) == 0) return 0;
     return -1;
 }

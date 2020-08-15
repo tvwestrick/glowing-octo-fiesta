7e40599122aa66ab6c3524129b4a5c24816d9496
tvwest <tvwest@umich.edu>
2020-08-15 17:57:21 -0400

    Made tests for remove spacers, don't know how to run them yet

--- src/equation_tests.cpp
+++ src/equation_tests.cpp
@@ -2,12 +2,11 @@
 #include <vector>
 #include <string>
 
+#include "equation.h" // ?? is this right ??
+
 using namespace std;
 
-bool tag_test1();
-
-
-
+void spaces_test();
 
 
 
@@ -16,10 +15,26 @@ int main() {
 }
 
 
-bool tag_test1() {
-    std::vector<int> left;
-    std::vector<int> right;
-    std::string eq = "1 + 5 + 4";
-    //NOT DONE YET
-    return 0;
+void spaces_test() {
+    std::vector<std::string> inputs1 = {"abc", " a b c ", "  a      b  c", " a       bc  "};
+    
+    for(int i = 0; i < inputs1.size(); ++i) {
+        elim_spaces(inputs1.at(i));
+        if(inputs1.at(i) == "abc")
+            std::cout << "Elim Spaces Test " << i << " PASSED\n";
+        else {
+            std::cout << "Elim Spaces Test " << i << " FAILED\n";
+        }
+    }
+    
+    std::vector<std::string> inputs2 = {"", "   ", "    "};
+    
+    for(int i = 0; i < inputs2.size(); ++i) {
+        elim_spaces(inputs2.at(i));
+        if(inputs2.at(i) == "")
+            std::cout << "Elim Spaces Test " << i+inputs1.size() << " PASSED\n";
+        else {
+            std::cout << "Elim Spaces Test " << i+inputs1.size() << " FAILED\n";
+        }
+    }
 }
